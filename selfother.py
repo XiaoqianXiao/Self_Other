ba35ca47e22e0072583ab160d427073be3ceb924
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2024-09-24 10:44:49 -0700

    change "pointing" with "pointer"

--- selfother.py
+++ selfother.py
@@ -26,14 +26,14 @@ detailed_intro_text = ('IN THE FOLLOWING TASK, YOU WILL BE ASKED TO RATE ONE OF 
                        'SELF: DOES THIS WORD DESCRIBE YOU? \n'
                        'OBAMA: DOES THIS WORD DESCRIBE FORMER U.S. PRESIDENT BARACK OBAMA?\n'
                        'UPPERCASE: IS THIS WORD PRINTED IN UPPERCASE LETTERS?\n\n'
-                       'FOR EACH ITEM, PLEASE RESPOND WITH YES [left pointing finger] OR NO [right pointing finger].\n'
+                       'FOR EACH ITEM, PLEASE RESPOND WITH YES [left pointer finger] OR NO [right pointer finger].\n'
                        'PLEASE KEEP YOUR EYES ON THE CENTRAL FIXATION POINT DURING THE TASK.\n\n'
                        'IF THE NEXT TRIAL COMES UP AND YOU HAVE NOT YET RESPONDED TO THE PREVIOUS TRIAL,\n'
                        'THAT IS OKAY, JUST SKIP IT AND MOVE ONTO THE NEXT ONE.\n\n'
                        'READY?')
 
 INSTRUCTIONS = {
-    'SCANNER': 'Please press left pointing finger yes \n \nand press right pointing finger for no',  # Instructions in scanner. Note that key name should match expInfo['setting']
+    'SCANNER': 'Please press left pointer finger yes \n \nand press right pointer finger for no',  # Instructions in scanner. Note that key name should match expInfo['setting']
     'PRACTICE': detailed_intro_text
 }
 
